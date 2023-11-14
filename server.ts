0e9a3b71c62f71ba816cb096e62bdf61dc558ac4
Zade Viggers <zade@viggers.net>
2023-11-14 18:40:55 +1300

    Table setup

--- server.ts
+++ server.ts
@@ -8,10 +8,29 @@ import { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 // We're just using SQLite since it's quick an easy
 const db = new DB("todos.db");
 
+// Database setup
+await db.execute(`CREATE TABLE IF NOT EXISTS "lists" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);`);
+
+// We can't do this concurrently because we need table relations
+await db.execute(`CREATE TABLE IF NOT EXISTS "items" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"list"	INTEGER NOT NULL,
+	"text"	TEXT NOT NULL,
+  -- SQLite just sees BOOLEAN as INTEGER, but it makes the schema clearer
+	"is_completed"	BOOLEAN NOT NULL,
+	"created_at"	INTEGER NOT NULL, 
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("list") REFERENCES "lists"("id")
+);`);
+
 // Itty-router is nice - Express-like router but with Request & Response
 const router = Router();
 
-router.get("/api/all-todos", () => {});
+router.get("/api/all-todos", async () => {});
 
 // File server stuff
 router
